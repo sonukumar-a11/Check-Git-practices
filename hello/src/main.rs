9cf357b7fb255c55ebf73ec8a7e600bbcdbdf52a
sonukumar-a11 <sonu.kumar@knoldus.com>
2021-03-11 23:22:57 +0530

    initial commit

---
+++ hello/src/main.rs
@@ -0,0 +1,13 @@
+mod risk;
+fn main() {
+    
+    let name: &str = "Sonu";
+    if risk::check_name(name)
+    {
+        println!("True");
+    }
+    else
+    {
+        println!("True");
+    }
+}
